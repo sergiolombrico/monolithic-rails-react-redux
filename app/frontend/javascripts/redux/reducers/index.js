192621afdace38c2da9def4b4345b4a6f6085a9b
Hunter Fortuin <hunter@flitti.com>
2016-11-11 13:28:48 -0500

    my first rails and react app

---
+++ app/frontend/javascripts/redux/reducers/index.js
@@ -0,0 +1,16 @@
+// app/frontend/javascripts/redux/reducers/index.js
+
+import { combineReducers } from 'redux';
+
+let reducer = (state = {}, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+const rootReducer = combineReducers({
+	reducer
+});
+
+export default rootReducer;
---
+++ app/frontend/javascripts/redux/store/index.js
@@ -0,0 +1,15 @@
+// app/frontend/javascripts/redux/store/index.js
+
+import { applyMiddleware, compose, createStore } from 'redux';
+import reducer from '../reducers';
+import logger from 'redux-logger';
+
+let finalCreateStore = compose(
+	applyMiddleware(
+		logger()
+	)
+)(createStore);
+
+export default function configureStore(initialState = {}) {
+	return finalCreateStore(reducer, initialState);
+}
